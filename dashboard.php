348df98bf84dc18b293a895cbbe0570f3430c15a
makis0189 <makis0189@gmail.com>
2026-09-02 06:49:00 -0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -142,7 +142,7 @@ $Highrisk_count= $high_row['total'];
 
 <aside class="home logout-btn">
 <i class="fa-solid fa-right-from-bracket"></i>
-<a href="logout.php">Logout</a>
+<button><a href="logout.php">Logout</a></button>
 </aside>
 </div>
 
