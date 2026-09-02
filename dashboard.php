8aa9129960301404944eb840421cd6bdb21c3d32
makis0189 <makis0189@gmail.com>
2026-09-02 07:03:39 -0700

    Update print statement from 'Hello' to 'Goodbye'

--- dashboard.php
+++ dashboard.php
@@ -142,7 +142,7 @@ $Highrisk_count= $high_row['total'];
 
 <aside class="home logout-btn">
 <i class="fa-solid fa-right-from-bracket"></i>
-<button class="logout"><a href="logout.php">Logout</a></button>
+<button class="home logout-btn"><a href="logout.php">Logout</a></button>
 </aside>
 </div>
 
