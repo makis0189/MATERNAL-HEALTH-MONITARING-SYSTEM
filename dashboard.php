a29ddec54d756a29767e49836b89dc6433d8be70
makis0189 <makis0189@gmail.com>
2026-09-07 08:45:10 -0700

    Update print statement to reflect farewell message

--- dashboard.php
+++ dashboard.php
@@ -226,7 +226,7 @@ $Highrisk_count= $high_row['total'];
 <input type="hidden" name="id">
 <div class="form-group">
 <label>Patient ID</label>
-<input type="text" name="patient_id" readonly>
+ <input type="text" id="patientId" name="patient_id" placeholder="e.g. MHS001" required>
 </div>
 <div class="form-group">
 <label>Full Name</label>
